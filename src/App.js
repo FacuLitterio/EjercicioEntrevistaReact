5bd0cfd57e079196bf289800cb4df296fe2ef20b
Facundo H. Litterio <facundolitterio7@gmail.com>
2021-02-25 10:08:34 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import {useState, useEffect} from 'react'
+
+//Styles
+import './styles.css';
+
+function App() {
+
+  const [contratistas, setContratistas] = useState([]);
+
+  const fetchContratistas = async () => {
+    const data = await fetch("https://smartcostpoc.azurewebsites.net/api/Contratistas?page=1&pageSize=10");
+    const contratistasData = await data.json();
+    console.log(contratistasData);
+    setContratistas(contratistasData);
+  }
+
+  useEffect(() => {
+    fetchContratistas();
+  }, [])
+
+  const orderHighToLow = () => {
+    const newArray = Array.from(contratistas)
+    const orderHigh = newArray.sort((a, b) => a.id - b.id);
+    setContratistas(orderHigh);
+  }
+
+  const orderLowToHigh = () => {
+    const newArray = Array.from(contratistas)
+    const orderLow = newArray.sort((a, b) => b.id - a.id);
+    setContratistas(orderLow);
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={orderHighToLow}>Ascendente</button>
+      <button type="button" onClick={orderLowToHigh}>Descendente</button>
+      <div className="App">
+        <ul className="list">
+        {
+          contratistas.map((contratista) => (
+            <li key={contratista.id}>
+              {contratista.id} - {contratista.cuit}
+            </li>
+          ))
+        }
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
